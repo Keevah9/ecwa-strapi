f1a2d2203e8ec412a570bdeb884c351443053bf5
Veronica Ogunniyi <veronica@icansee.co.uk>
2024-04-14 12:54:28 +0100

    heroslider to homepage block

--- config/plugins.js
+++ config/plugins.js
@@ -18,7 +18,6 @@ module.exports = ({ env }) => ({
             providerOptions: {
                 cloud_name: env('CLOUDINARY_NAME'),
                 api_key: env('CLOUDINARY_KEY'),
-                sizeLimit: 250 * 1024 * 1024,
                 api_secret: env('CLOUDINARY_SECRET'),
             },
             actionOptions: {
